1363ada570d8bd66a44a53fb0a4dcd202a0f2e09
Scott Livingston <slivingston@caltech.edu>
2012-04-04 17:11:36 -0700

    Bump version.

--- btsynth/__init__.py
+++ btsynth/__init__.py
@@ -2,7 +2,7 @@
 SCL; 2011, 2012.
 """
 
-version = "0.1"
+version = "0.2"
 
 from btsynth import *
 from gridworld import *
--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, 2012, California Institute of Technology'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
